886c73f72ecd36f34ba8cc7c7516ee26a7513a36
Sriraj Muddu <muddusriraj@gmail.com>
2025-01-13 20:31:26 -0500

    Add files via upload

---
+++ MatrixOperations/graph.h
@@ -0,0 +1,16 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include <vector>
+#include "operators.h"
+
+
+void drawArrow(double x, double y, double angle);
+
+void drawText(const char* text, double x, double y);
+
+void drawVector(const std::vector<double>& vec, const std::string color);
+
+void plot(const std::vector<darray>& vectors, const std::vector<std::string> colors = {"blue"});
+
+
+#endif
